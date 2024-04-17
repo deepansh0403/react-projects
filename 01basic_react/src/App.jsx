79ecac755f1215b453ab01f86c5502d61687d13c
deepanshu <deepanshubisht330@gmail.cpm>
2024-04-17 13:04:24 +0530

    react first app

---
+++ 01basic_react/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <h1>Hello React with Vite and now</h1>
+      <p>i want to commit this project to github</p>
+    </>
+  );
+};
+
+export default Home;
