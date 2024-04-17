4658740ba64463debf03e9a6060234ae1c35dce6
deepanshu <deepanshubisht330@gmail.cpm>
2024-04-17 13:07:17 +0530

    new changes for checking

--- 01basic_react/src/App.jsx
+++ 01basic_react/src/App.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <>
       <h1>Hello React with Vite and now</h1>
-      <p>i want to commit this project to github</p>
+      <p>i want to commit this project to github again im trying to change new changes</p>
     </>
   );
 };
